a3af974b46c8a026a5afb302bb15d0e16de89f1c
beyzauenal <beyza.uenal@student.htldornbirn.at>
2024-10-02 08:04:15 +0200

    ifbedienung2 done

---
+++ src/IfBedingungen2.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+
+public class IfBedingungen2  {
+    public static void main(String[] args) {
+        // Zufallszahlen erstellen zwischen 0-100
+        Random random = new Random();
+        int randomnumber = random.nextInt(100);
+
+        // Gib die Zufallszahl aus
+        System.out.println(randomnumber);
+
+        // Wenn die Zahl kleiner ist als 20, gib "Mini" aus
+        // Wenn die Zahl zwischen 20 und 50 ist, gib "Medium" aus
+        // Wenn die Zahl größer als 50 ist, gib "Large" aus
+        if (randomnumber < 20) {
+            System.out.println("Mini");
+        } else if (randomnumber >= 20 && randomnumber <= 50) {
+            System.out.println("Medium");
+
+        } else {
+            System.out.println("Large");
+        }
+    }
+}
