6fdae6d6edb6fba5fc1a6b10fc77c9ebf6b3dda2
beyzauenal <beyza.uenal@student.htldornbirn.at>
2024-10-24 08:04:12 +0200

    Wörterraten_endoflesson

--- src/at/beyza/projects/basics/strings/Wörterratenprocess.java
+++ src/at/beyza/projects/basics/strings/Wörterratenprocess.java
@@ -1,16 +1,17 @@
-package strings;
+package at.beyza.projects.basics.strings;
 
 import java.util.Random;
-import java.util.Scanner;
 
 public class Wörterratenprocess {
     public static void main(String[] args) {
-        String[] woerter = {"Cäsar", "Programm", "Java", "Maus", "Auto"};
+        String[] woerter = {"Cäsar", "Programm "};
         Random random = new Random();
         String geheimWort = woerter[random.nextInt(woerter.length)];
         String output = "";
 
         for (int i = 0; i < geheimWort.length(); i++) {
-            output += "*";
+            output += "";
         }
+    }
+}
 
