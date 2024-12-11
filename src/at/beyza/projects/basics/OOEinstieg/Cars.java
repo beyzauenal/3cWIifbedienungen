5114f78944f50b115812117f0f9c6bc12501a48c
beyzauenal <beyza.uenal@student.htldornbirn.at>
2024-12-11 08:09:19 +0100

    04 Getter & Setter-process

--- src/at/beyza/projects/basics/OOEinstieg/Cars.java
+++ src/at/beyza/projects/basics/OOEinstieg/Cars.java
@@ -1,59 +1,74 @@
 package at.beyza.projects.basics.OO;
 
 public class Cars {
-    // Instanz/Gedächtnisvariablen
-    private int carSpritverbrauch;
-    private String carcolor;
-    private String carserialNumber;
-    private int fuelAmount;  // fuelAmount als int deklariert
-    private String brand;
+    // Instanzvariablen
+    private int carKonsumption; // Verbrauch pro Einheit
+    private String carColor; // Farbe des Autos
+    private String carSerialNumber; // Seriennummer
+    private int fuelAmount; // Verbleibender Kraftstoff
+    private String brand; // Marke des Autos
 
     // Konstruktor
-    public Cars(int fc, String b, String s, int fa) {
-        this.carSpritverbrauch = fc;
-        this.carcolor = b;
-        this.carserialNumber = s;
-        this.fuelAmount = fa;
+    public Cars(int carKonsumption, String carColor, String carSerialNumber, int fuelAmount) {
+        this.carKonsumption = carKonsumption;
+        this.carColor = carColor;
+        this.carSerialNumber = carSerialNumber;
+        this.fuelAmount = fuelAmount;
+        this.brand = "Unknown"; // Standardwert für die Marke
     }
 
     // Methode zum Fahren
     public void drive() {
-        this.fuelAmount = this.fuelAmount - carSpritverbrauch;
-        System.out.println("I'm driving");
+        if (fuelAmount >= carKonsumption) {
+            fuelAmount -= carKonsumption;
+            System.out.println("I'm driving.");
+        } else {
+            System.out.println("Not enough fuel to drive.");
+        }
     }
 
     // Methode zum Bremsen
-    public void dobreak() {
-        System.out.println("Ich bremse");
+    public void doBreak() {
+        System.out.println("Ich bremse.");
     }
 
     // Methode für TurboBoost
     public void turboBoost() {
-        if (fuelAmount > carSpritverbrauch * 0.1) {
-            System.out.println("SuperBoostMode");
+        if (fuelAmount > carKonsumption * 0.1) {
+            System.out.println("SuperBoostMode activated!");
         } else {
-            System.out.println("Not enough fuel to go to Superboost");
+            System.out.println("Not enough fuel to activate SuperBoost.");
         }
     }
 
-    // Methode für Wiederholungen
+    // Methode zur Berechnung der verbleibenden Reichweite
+    public void getRemainingRange() {
+        if (carKonsumption > 0) {
+            int range = fuelAmount / carKonsumption;
+            System.out.println("Remaining range: " + range + " units.");
+        } else {
+            System.out.println("Invalid consumption value. Cannot calculate range.");
+        }
+    }
+
+    // Methode für Wiederholungen (neu hinzugefügt)
     public void amountOfRepetitions(int repetition) {
-        for (int i = 0; i < repetition; i++) {
-            System.out.println("This is repetition number " + (i + 1));
+        for (int i = 1; i <= repetition; i++) {
+            System.out.println("This is repetition number " + i);
         }
     }
 
     // Getter-Methoden
-    public int getCarSpritverbrauch() {
-        return carSpritverbrauch;
+    public int getCarKonsumption() {
+        return carKonsumption;
     }
 
-    public String getCarcolor() {
-        return carcolor;
+    public String getCarColor() {
+        return carColor;
     }
 
-    public String getCarserialNumber() {
-        return carserialNumber;
+    public String getCarSerialNumber() {
+        return carSerialNumber;
     }
 
     public int getFuelAmount() {
@@ -63,5 +78,13 @@ public class Cars {
     public String getBrand() {
         return brand;
     }
-}
 
+    // Setter-Methoden (falls benötigt)
+    public void setFuelAmount(int fuelAmount) {
+        this.fuelAmount = fuelAmount;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+}
--- src/at/beyza/projects/basics/OOEinstieg/ErsteObjekt.java
+++ src/at/beyza/projects/basics/OOEinstieg/ErsteObjekt.java
@@ -2,18 +2,17 @@ package at.beyza.projects.basics.OO;
 
 public class ErsteObjekt {
     public static void main(String[] args) {
-
-        int myNumber = 7;
-
+        // Erstellen eines Car-Objekts
         Cars c1 = new Cars(8, "grey", "BEZAUI", 80);
 
+        // Test der Methoden
         c1.dobreak();
         c1.turboBoost();
         c1.amountOfRepetitions(3);
         c1.getRemainingRange();
 
-        Cars c2 = new Cars(17, "open", "1234", 50);
-        System.out.println(c2.getCarserialNumber());  // Verwende den Getter für carserialNumber
-
+        // Erstellen eines weiteren Car-Objekts
+        Cars c2 = new Cars(17, "red", "1234", 50);
+     m   System.out.println("Serial Number of car 2: " + c2.getCarserialNumber()); // Ausgabe der Seriennummer
     }
 }
