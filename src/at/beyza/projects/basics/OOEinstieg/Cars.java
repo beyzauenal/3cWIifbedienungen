f351b077a9d4bdccee630d3b450f4fa3258fb7a2
beyzauenal <beyza.uenal@student.htldornbirn.at>
2024-12-12 08:00:29 +0100

    04 Getter & Setter-process

--- src/at/beyza/projects/basics/OOEinstieg/Cars.java
+++ src/at/beyza/projects/basics/OOEinstieg/Cars.java
@@ -2,69 +2,95 @@ package at.beyza.projects.basics.OO;
 
 public class Cars {
     // Instanzvariablen
-    private int carKonsumption; // Verbrauch pro Einheit
+    private int fuelUsage; // Verbrauch pro Einheit
     private String carColor; // Farbe des Autos
     private String carSerialNumber; // Seriennummer
-    private int fuelAmount; // Verbleibender Kraftstoff
-    private String brand; // Marke des Autos
+
+    private int amountOfRepetitions; // Anzahl der Wiederholungen
+    private int remainingRange; // Verbleibende Reichweite
+    private int fuelAmount; // Aktueller Kraftstoff
 
     // Konstruktor
-    public Cars(int carKonsumption, String carColor, String carSerialNumber, int fuelAmount) {
-        this.carKonsumption = carKonsumption;
-        this.carColor = carColor;
-        this.carSerialNumber = carSerialNumber;
-        this.fuelAmount = fuelAmount;
-        this.brand = "Unknown"; // Standardwert für die Marke
+    public Cars(int carConsumption, String carColor, String carSerialNumber, int fuelAmount) {
+        this.fuelUsage = carConsumption; // Initialisierung der Verbrauchswerte
+        this.carColor = carColor; // Initialisierung der Farbe
+        this.carSerialNumber = carSerialNumber; // Initialisierung der Seriennummer
+        this.fuelAmount = fuelAmount; // Initialisierung des Kraftstoffs
     }
 
     // Methode zum Fahren
     public void drive() {
-        if (fuelAmount >= carKonsumption) {
-            fuelAmount -= carKonsumption;
-            System.out.println("I'm driving.");
-        } else {
-            System.out.println("Not enough fuel to drive.");
+        if (fuelAmount > 0) { // Logischer Vergleich, ob genug Kraftstoff vorhanden ist
+            fuelAmount -= fuelUsage; // Verbrauch reduzieren
+            // System.out.println("I'm driving");
         }
     }
 
-    // Methode zum Bremsenn
+    // Methode zum Bremsen
     public void doBreak() {
-        System.out.println("Ich bremse.");
+        // System.out.println("Ich bremse.");
     }
 
     // Methode für TurboBoost
     public void turboBoost() {
-        if (fuelAmount > carKonsumption * 0.1) {
-            System.out.println("SuperBoostMode activated!");
+        if (fuelAmount > fuelUsage * 0.1) { // Bedingung für den TurboBoost
+            System.out.println("SuperBoostMode");
         } else {
             System.out.println("Not enough fuel to activate SuperBoost.");
         }
     }
 
-    // Methode zur Berechnung der verbleibenden Reichweite
-    public void getRemainingRange() {
-        if (carKonsumption > 0) {
-            int range = fuelAmount / carKonsumption;
-            System.out.println("Remaining range: " + range + " units.");
-        } else {
-            System.out.println("Invalid consumption value. Cannot calculate range.");
+    // Methode zur Berechnung der Wiederholungen
+    public void amountOfRepetitions(int repetition) {
+        for (int i = 0; i < repetition; i++) {
+            // System.out.println("Tutututut");
         }
     }
 
-    // Methode für Wiederholungen (neu hinzugefügt)
-    public void amountOfRepetitions(int repetition) {
-        for (int i = 1; i <= repetition; i++) {
-            System.out.println("This is repetition number " + i);
+    // Methode für die verbleibende Reichweite
+    public void getRemainingRange() {
+        if (fuelUsage > 0) { // Verhindert Division durch null
+            remainingRange = fuelAmount / fuelUsage;
         }
+        // System.out.println(remainingRange);
+    }
+
+    // Setter-Methoden
+    public void setFuelUsage(int fuelUsage) {
+        this.fuelUsage = fuelUsage;
+    }
+
+    public void setCarColor(String carColor) {
+        this.carColor = carColor;
+    }
+
+    public void setCarSerialNumber(String carSerialNumber) {
+        this.carSerialNumber = carSerialNumber;
+    }
+
+    public void setFuelAmount(int fuelAmount) {
+        if (fuelAmount > 100) { // Beschränkung der maximalen Kraftstoffmenge
+            this.fuelAmount = 100;
+        } else {
+            this.fuelAmount = fuelAmount;
+        }
+    }
+
+    public void setAmountOfRepetitions(int amountOfRepetitions) {
+        this.amountOfRepetitions = amountOfRepetitions;
+    }
+
+    public void setRemainingRange(int remainingRange) {
+        this.remainingRange = remainingRange;
     }
 
     // Getter-Methoden
-    public int getCarKonsumption() {
-        return carKonsumption;
-    }
-
     public String getCarColor() {
         return carColor;
+    }
+
+    public int getFuelUsage() {
+        return fuelUsage;
     }
 
     public String getCarSerialNumber() {
@@ -75,16 +101,12 @@ public class Cars {
         return fuelAmount;
     }
 
-    public String getBrand() {
-        return brand;
+    public int getAmountOfRepetitions() {
+        return amountOfRepetitions;
     }
 
-    // Setter-Methoden (falls benötigt)
-    public void setFuelAmount(int fuelAmount) {
-        this.fuelAmount = fuelAmount;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
+    public int getRemainingRange() {
+        return remainingRange;
     }
 }
+
--- src/at/beyza/projects/basics/OOEinstieg/ErsteObjekt.java
+++ src/at/beyza/projects/basics/OOEinstieg/ErsteObjekt.java
@@ -4,15 +4,22 @@ public class ErsteObjekt {
     public static void main(String[] args) {
         // Erstellen eines Car-Objekts
         Cars c1 = new Cars(8, "grey", "BEZAUI", 80);
+        c1.setFuelAmount(80);  // Kraftstoffmenge setzen
 
-        // Test der Methoden
-        c1.dobreak();
+        System.out.println(c1.getFuelAmount());  // Aktuellen Kraftstoffstand ausgeben
+
+        c1.doBreak();
         c1.turboBoost();
         c1.amountOfRepetitions(3);
-        c1.getRemainingRange();
+        c1.getRemainingRange();  // Methode korrekt aufrufen
 
-        // Erstellen ein weiteres Car-Objekts
+        // Zweites Auto
         Cars c2 = new Cars(17, "red", "1234", 50);
-     m   System.out.println("Serial Number of car 2: " + c2.getCarserialNumber()); // Ausgabe der Seriennummer
+
+        System.out.println(c2.getCarColor());  // c1 auf c2 ändern, da c2 instanziiert wurde
+
+        System.out.println(c2.getCarSerialNumber());
+        c2.drive();
+        System.out.println(c2.getFuelAmount());
     }
 }
