73712163d8783f485850abdfc2aca772dafa2922
beyzauenal <beyza.uenal@student.htldornbirn.at>
2024-12-30 18:38:26 +0100

    10 Mini-Projekt Fernbedienung  - done

---
+++ src/at/beyza/projects/basics/ooEinstieg/Main2.java
@@ -0,0 +1,15 @@
+package at.beyza.projects.basics.ooEinstieg;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Fernbedienung remoteControl = new Fernbedienung(100, 100); // Beide Batterien starten mit 100% Ladung
+
+        System.out.println("Ladestatus: " + remoteControl.getStatus() + "%");
+
+        remoteControl.turnOn(); // Verbraucher wird angeschlossen
+        System.out.println("Ladestatus nach Einschalten: " + remoteControl.getStatus() + "%");
+
+        remoteControl.turnOff(); // Verbraucher wird ausgeschaltet
+        System.out.println("Ladestatus nach Ausschalten: " + remoteControl.getStatus() + "%");
+    }
+}
