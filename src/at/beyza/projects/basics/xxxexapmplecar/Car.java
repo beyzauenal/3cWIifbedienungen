33371e9a9f08903632ecba5e0de82e0f4e61e58c
beyzauenal <beyza.uenal@student.htldornbirn.at>
2025-01-15 08:24:45 +0100

    11 Aufgabe Autos-done

---
+++ src/at/beyza/projects/basics/xxxexapmplecar/Car.java
@@ -0,0 +1,43 @@
+package at.beyza.projects.basics.xxxexapmplecar;
+
+public class Car {
+    private String color;
+    private double maxSpeed;
+    private double basePrice;
+    private double baseConsumption;
+    private Manufacturer manufacturer;
+    private Engine engine;
+
+    public Car(String color, double maxSpeed, double basePrice, double baseConsumption, Manufacturer manufacturer, Engine engine) {
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.basePrice = basePrice;
+        this.baseConsumption = baseConsumption;
+        this.manufacturer = manufacturer;
+        this.engine = engine;
+    }
+
+    public double calculatePrice() {
+        return basePrice - (basePrice * manufacturer.getDiscountPercentage() / 100);
+    }
+
+    public double calculateConsumption(double kilometersDriven) {
+        if (kilometersDriven <= 50000) {
+            return baseConsumption;
+        }
+        return baseConsumption * 1.098;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", maxSpeed=" + maxSpeed +
+                ", price=" + calculatePrice() +
+                ", consumption (base)=" + baseConsumption +
+                ", manufacturer=" + manufacturer.getName() +
+                ", engine=" + engine.getType() +
+                " (" + engine.getPower() + " HP)" +
+                '}';
+    }
+}
---
+++ src/at/beyza/projects/basics/xxxexapmplecar/Main.java
@@ -0,0 +1,28 @@
+package at.beyza.projects.basics.xxxexapmplecar;
+
+public class Main {
+    public static void main(String[] args) {
+        Manufacturer vw = new Manufacturer("Volkswagen", "Germany", 10);
+        Engine dieselEngine = new Engine("Diesel", 150);
+        Car vwCar = new Car("Red", 220, 30000, 6.5, vw, dieselEngine);
+
+        System.out.println("Auto-Details (Volkswagen):");
+        System.out.println(vwCar);
+        System.out.println("Verbrauch nach 60,000 km: " + vwCar.calculateConsumption(60000) + " L/100km\n");
+
+        Manufacturer beyzaManufacturer = new Manufacturer("Beyza", "Austria", 15);
+        Engine petrolEngine = new Engine("Petrol", 200);
+        Car beyzaCar = new Car("Blue", 240, 35000, 7.0, beyzaManufacturer, petrolEngine);
+
+        System.out.println("Auto-Details (Beyza):");
+        System.out.println(beyzaCar);
+        System.out.println("Verbrauch nach 60,000 km: " + beyzaCar.calculateConsumption(60000) + " L/100km");
+    }
+}
+
+/// src/
+//└── at/beyza/projects/basics/xxxexapmplecar/
+//    ├── Manufacturer.java
+//    ├── Engine.java
+//    ├── Car.java
+//    └── Main.java
