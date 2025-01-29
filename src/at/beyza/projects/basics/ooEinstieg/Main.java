85d503ebb576208fee0f7d45d11f0579fc63b0d8
beyzauenal <beyza.uenal@student.htldornbirn.at>
2025-01-29 18:39:01 +0100

    12 Vererbung- done

--- src/at/beyza/projects/basics/ooEinstieg/Main.java
+++ src/at/beyza/projects/basics/ooEinstieg/Main.java
@@ -4,18 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
         // Initialisierung der Objekte
-        engine e1 = new engine(140, engine.TYPE.DIESEL);
-        RearMirror r1 = new RearMirror(100, 0);
-        RearMirror r2 = new RearMirror(90, 40);
+        engine e1 = new engine(140, engine.TYPE.DIESEL);  // Engine-Objekt
+        RearMirror r1 = new RearMirror(100, 0);  // Rückspiegel 1
+        RearMirror r2 = new RearMirror(90, 40);  // Rückspiegel 2
 
-        //Reifen erstellen
+        // Reifen erstellen
         Reifen reifen1 = new Reifen(17, "Sommer");
         Reifen reifen2 = new Reifen(17, "Sommer");
         Reifen reifen3 = new Reifen(17, "Sommer");
         Reifen reifen4 = new Reifen(17, "Sommer");
 
-        // Konstruktor der Cars-Klasse erfordert mehr Parameter
-        Cars c1 = new Cars(e1, "red", "123ABC", 50, 5); // Beispielwerte für carSerialNumber und fuelAmount
+        // Konstruktor der Car-Klasse erfordert mehr Parameter
+        Car c1 = new Car(e1, "red", "123ABC", 50, 5); // Beispielwerte für carSerialNumber und fuelAmount
         c1.addMirror(r1);
         c1.addMirror(r2);
 
@@ -26,27 +26,12 @@ public class Main {
 
         // Ausgabe der Position des ersten Rückspiegels und der PS des Motors
         System.out.println(c1.getMirrors().get(0).getPosition());
-        System.out.println(c1.getengine().getHorsePower());
+        System.out.println(c1.getEngine().getHorsePower());
         System.out.println("Das Auto hat " + c1.getMirrors().size() + " Rückspiegel.");
         System.out.println("Das Auto hat " + c1.getReifen().size() + " Reifen.");
 
-
-        /*
-        // Auskommentierter Code muss überprüft werden, da die Methoden wie doBreak(), turboBoost() und andere fehlen
-        Cars c1 = new Cars(8, "grey", "BEZAUI", 80);
-        c1.setFuelAmount(80);
-        System.out.println("Kraftstoffstand c1: " + c1.getFuelAmount());
-
-        c1.doBreak();
-        c1.turboBoost();
-        c1.amountOfRepetitions(3);
-        System.out.println("Verbleibende Reichweite c1: " + c1.getRemainingRange());
-
-        Cars c2 = new Cars(17, "red", "1234", 50);
-        System.out.println("Farbe von c2: " + c2.getCarColor());
-        System.out.println("Seriennummer von c2: " + c2.getCarSerialNumber());
-        c2.drive();
-        System.out.println("Kraftstoffstand c2 nach Fahrt: " + c2.getFuelAmount());
-        */
+        // Testen Truck
+        Truck t1 = new Truck(e1, "Mercedes", "Green", "Trailer1");
+        System.out.println("Colour: " + t1.getCarColor() + ", Trailer: " + t1.getTrailer());
     }
 }
---
+++ src/at/beyza/projects/basics/ooEinstieg/Truck.java
@@ -0,0 +1,18 @@
+package at.beyza.projects.basics.ooEinstieg;
+
+public class Truck extends Car {
+    private String trailer;
+
+    public Truck(engine engine, String brand, String colour, String trailer) {
+        super(engine, brand, colour, 50, 5); // Beispielwerte für carSerialNumber und fuelAmount
+        this.trailer = trailer;
+    }
+
+    public String getTrailer() {
+        return trailer;
+    }
+
+    public void setTrailer(String trailer) {
+        this.trailer = trailer;
+    }
+}
